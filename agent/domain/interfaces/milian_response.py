0225c6f3560df19fc33dfacc5b5ffc220b28baa1
haochigou <14619091+haochigou@user.noreply.gitee.com>
2024-12-05 19:13:48 +0800

    更新提示

--- agent/domain/interfaces/milian_response.py
+++ agent/domain/interfaces/milian_response.py
@@ -7,6 +7,8 @@ from agent.domain.entities.chat import ChatRequest
 from agent.app.chat_service import ChatService
 from agent.infra.utils.packed_ulid import get_packed_ulid
 
+recommand = {"情感陪伴——放松舒缓催眠冥想", "JIMI肌密公主痛经贴", "凯格尔运动——核心训练", "私密花园——黄金三角区激活", "身体魅力训练——自由舞动", "爱道灵动运动——108动"}
+
 def detect_content_type(content: str):
     start = content.find("{")
     if start >= 0:
@@ -36,7 +38,7 @@ async def get_milian_response(traceId: str, chat_service: ChatService):
             print(raw_response)
             if raw_response["finish_reason"] is not None:
                 print(cards)
-                chat_service._chat_response.content = total + (f"已推荐[{cards}]后面不需要再推荐了。" if cards else "")
+                chat_service._chat_response.content = total + (f"\n自我提醒：我已经为用户生成了cardTag为：{cards} 的推荐内容，后面无论用户怎么表达，我都不能再生成同样的内容，否则用户会厌烦。" if cards else "")
                 print(chat_service._chat_response.content)
             content = raw_response["content"]
             pre_content = None
@@ -60,6 +62,10 @@ async def get_milian_response(traceId: str, chat_service: ChatService):
                 is_start = "false"
                 if type != 1 and type != 5:
                     item = json.loads(content.replace("\\\"","\""))
+                    if item["cardTag"] not in recommand:
+                        continue
+                    if any(history["content"].find(item["cardTag"]) >= 0 for history in chat_service._chat_history.get_context(15)):
+                        continue
                     if cards is None:
                         cards = str(item["cardTag"])
                     else:
